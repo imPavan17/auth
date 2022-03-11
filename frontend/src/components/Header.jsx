f0a2188106c8491a6fcdc1ec934c18be287fce6e
Pavan <pavans@geekyants.com>
2022-03-11 15:58:22 +0530

    ✨ Header & Initial pages

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="header">
+      <Link to="/">Support Desk</Link>
+
+      <ul>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
