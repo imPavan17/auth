20e3c24754372629946a451a2b2df08da6638ee2
Pavan <pavans@geekyants.com>
2022-03-13 23:13:57 +0530

    ✨ CORS setup

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,17 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// CORS SETUP
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 // Routes
 app.use("/api/users", UserRoutes);
 app.use("/api", TodoRoutes);
