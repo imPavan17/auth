8f69576085575c43f083136357a9f22806186306
Pavan <pavans@geekyants.com>
2022-03-13 21:34:10 +0530

    ✨ integrated pagination | Todos

--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -5,10 +5,19 @@ const Todo = require("../models/todo");
 const router = express.Router();
 
 router.get("/todos", protect, async (req, res) => {
+  const ITEMS_PER_PAGE = 2;
+  const { page } = req.query; // 1, 2, 3...
   try {
-    const todos = await Todo.findAll();
+    const count = await Todo.count(); // returns occurrences of elements in the database
+    const todos = await Todo.findAll({
+      offset: (page - 1) * ITEMS_PER_PAGE,
+      limit: ITEMS_PER_PAGE,
+    });
     res.status(200);
-    res.json(todos);
+    res.json({
+      data: todos,
+      count,
+    });
   } catch (err) {
     res.status(500);
     throw new Error("Something went wrong!");
